af57a9e74477dab404887e8e878b10cf097cf3c3
Kai Katsumata <imperialroyalguard38@gmail.com>
2024-10-17 08:08:58 +0900

    debug

--- pipeline_flux_rf_inversion.py
+++ pipeline_flux_rf_inversion.py
@@ -555,12 +555,12 @@ class FluxRFInversionPipeline(DiffusionPipeline, FluxLoraLoaderMixin):
         else:
             image_latents = torch.cat([image_latents], dim=0)
         noise = randn_tensor(shape, generator=generator, device=device, dtype=dtype)
-        latents = self.scheduler.scale_noise(image_latents, timestep, noise)        
-        latents = self._pack_latents(latents, batch_size, num_channels_latents, height, width)
-        image_latents = self._pack_latents(image_latents, batch_size, num_channels_latents, height, width)
-
+        # latents = self.scheduler.scale_noise(image_latents, timestep, noise)        
+        # latents = self._pack_latents(latents, batch_size, num_channels_latents, height, width)
         # image_latents = self._pack_latents(image_latents, batch_size, num_channels_latents, height, width)
-        # latents = self.controlled_forward_ode(image_latents, latent_image_ids, sigmas, gamma=gamma, null_prompt_embeds=null_prompt_embeds, null_pooled_prompt_embeds=null_pooled_prompt_embeds, null_text_ids=null_text_ids)
+
+        image_latents = self._pack_latents(image_latents, batch_size, num_channels_latents, height, width)
+        latents = self.controlled_forward_ode(image_latents, latent_image_ids, sigmas, gamma=gamma, null_prompt_embeds=null_prompt_embeds, null_pooled_prompt_embeds=null_pooled_prompt_embeds, null_text_ids=null_text_ids)
 
         ori_image_latents = image_latents.clone()
         return ori_image_latents, latents, latent_image_ids
