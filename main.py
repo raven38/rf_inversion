30eca0a652a2434f7ab9fa9a5299b7cf32b7ea93
Kai Katsumata <imperialroyalguard38@gmail.com>
2024-10-17 04:29:59 +0900

    Add main.py with FluxRFInversionPipeline usage

---
+++ main.py
@@ -0,0 +1,19 @@
+import torch
+
+from diffusers import FluxRFInversionPipeline
+from diffusers.utils import load_image
+
+device = "cuda"
+pipe = FluxRFInversionPipeline.from_pretrained("black-forest-labs/FLUX.1-schnell", torch_dtype=torch.bfloat16)
+pipe = pipe.to(device)
+
+url = "https://raw.githubusercontent.com/CompVis/stable-diffusion/main/assets/stable-samples/img2img/sketch-mountains-input.jpg"
+init_image = load_image(url).resize((1024, 1024))
+
+prompt = "cat wizard, gandalf, lord of the rings, detailed, fantasy, cute, adorable, Pixar, Disney, 8k"
+
+images = pipe(
+    prompt=prompt, image=init_image, num_inference_steps=4, strength=0.95, guidance_scale=0.0
+).images[0]
+
+images[0].save("output.jpg")
