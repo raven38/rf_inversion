05a72295a1d649610ace02bd1783483f6528263c
Kai Katsumata <imperialroyalguard38@gmail.com>
2024-10-18 01:26:08 +0900

    debug

--- pipeline_flux_rf_inversion.py
+++ pipeline_flux_rf_inversion.py
@@ -861,7 +861,7 @@ class FluxRFInversionPipeline(DiffusionPipeline, FluxLoraLoaderMixin):
         with self.progress_bar(total=num_inference_steps) as progress_bar:
             y_0 = ori_latents.clone()
             for i, t in enumerate(timesteps):
-                t_i = torch.tensor(1 - ((i+1) / (len(timesteps))), device=device)
+                t_i = timestep / 1000 # torch.tensor(1 - ((i+1) / (len(timesteps))), device=device)
                 dt = torch.tensor(1 / (len(timesteps)), device=device)
                 if self.interrupt:
                     continue
