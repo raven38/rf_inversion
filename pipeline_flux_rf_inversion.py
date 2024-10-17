e9c42c9ff0710ee44e0904f1ee6e1cf76a66274c
Kai Katsumata <imperialroyalguard38@gmail.com>
2024-10-17 10:16:35 +0900

    debug

--- pipeline_flux_rf_inversion.py
+++ pipeline_flux_rf_inversion.py
@@ -868,7 +868,7 @@ class FluxRFInversionPipeline(DiffusionPipeline, FluxLoraLoaderMixin):
                 timestep = t.expand(latents.shape[0]).to(latents.dtype)
                 v_t = -self.transformer(
                     hidden_states=latents,
-                    timestep=timestep / 1000,
+                    timestep=torch.tensor(t_i, dtype=latents.dtype, device=device).repeat(batch_size),
                     guidance=guidance,
                     pooled_projections=pooled_prompt_embeds,
                     encoder_hidden_states=prompt_embeds,
