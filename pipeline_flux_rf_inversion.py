9ab610a8e245d11bddbc1fcd0123177ce9e79813
Kai Katsumata <imperialroyalguard38@gmail.com>
2024-10-23 01:24:39 +0900

    debug image save

--- pipeline_flux_rf_inversion.py
+++ pipeline_flux_rf_inversion.py
@@ -928,6 +928,13 @@ class FluxRFInversionPipeline(DiffusionPipeline, FluxLoraLoaderMixin):
 
                 # latents = self.scheduler.step(v_t, t, latents, return_dict=False)[0]
 
+                # debug print save image 
+                debug_latents = self._unpack_latents(latents, height, width, self.vae_scale_factor)
+                deubg_latents = (debug_latents / self.vae.config.scaling_factor) + self.vae.config.shift_factor
+                debug_image = self.vae.decode(debug_latents, return_dict=False)[0]
+                debug_image = self.image_processor.postprocess(debug_image, output_type=output_type)
+                debug_image.save(f"debug_{i}.png")
+
                 if latents.dtype != latents_dtype:
                     if torch.backends.mps.is_available():
                         # some platforms (eg. apple mps) misbehave due to a pytorch bug: https://github.com/pytorch/pytorch/pull/99272
