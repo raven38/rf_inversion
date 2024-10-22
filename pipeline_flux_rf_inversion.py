39bb1df93337f2a85812f7399affc9103fb9dbcd
Kai Katsumata <imperialroyalguard38@gmail.com>
2024-10-23 01:15:05 +0900

    debug inversion

--- pipeline_flux_rf_inversion.py
+++ pipeline_flux_rf_inversion.py
@@ -919,7 +919,7 @@ class FluxRFInversionPipeline(DiffusionPipeline, FluxLoraLoaderMixin):
                 # diffusion = torch.sqrt(2 * (1-t_i) * (1-eta) / t_i)
                 # noise = torch.randn_like(latents)
 
-                latents = latents + v_hat_t * (sigmas[i] - sigmas[i+1])
+                latents = latents + v_hat_t * (sigmas[i + 1] - sigmas[i])
                 # print(t_i, timestep / 1000, dt, eta_t, v_t.mean().item(), latents.mean().item(), v_hat_t.mean().item(), diffusion.mean().item(), noise.mean().item())
                 # if start_timestep <= i < stop_timestep:
                 #    latents = latents + v_hat_t * dt + diffusion * torch.sqrt(dt) * noise
