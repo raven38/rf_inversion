235be0eff03c3fcfc24cfc540c3cffbee976f12d
Kai Katsumata <imperialroyalguard38@gmail.com>
2024-10-23 16:09:02 +0900

    debug

--- pipeline_flux_rf_inversion.py
+++ pipeline_flux_rf_inversion.py
@@ -618,7 +618,7 @@ class FluxRFInversionPipeline(DiffusionPipeline, FluxLoraLoaderMixin):
             # Y_t = Y_t + u_hat_t_i * dt +  torch.sqrt(dt) * diffusion * noise 
 
             # update Y_t
-            Y_t = Y_t + u_hat_t_i * (sigmas[i] - sigmas[i+1])
+            Y_t = Y_t + u_hat_t_i * (sigmas[i+1] - sigmas[i])
 
         return Y_t
 
