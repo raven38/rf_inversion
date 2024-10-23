8deac270ea59ae3c1f9507a98e2885aef1d62f9e
Kai Katsumata <imperialroyalguard38@gmail.com>
2024-10-23 13:57:51 +0900

    debug

--- pipeline_flux_rf_inversion.py
+++ pipeline_flux_rf_inversion.py
@@ -889,8 +889,8 @@ class FluxRFInversionPipeline(DiffusionPipeline, FluxLoraLoaderMixin):
         # 6. Denoising loop
         with self.progress_bar(total=num_inference_steps) as progress_bar:
             y_0 = ori_latents.clone()
-            for i, t in enumerate(self.scheduler.timesteps[2:]):
-                t_i = t / 1000 # torch.tensor(1 - ((i+1) / (len(timesteps))), device=device)
+            for i, t in enumerate(reversed(self.scheduler.timesteps)):
+                t_i = 1 - t / 1000 # torch.tensor(1 - ((i+1) / (len(timesteps))), device=device)
                 print(t_i)
                 dt = torch.tensor(1 / (len(timesteps) - 1), device=device)
                 if self.interrupt:
