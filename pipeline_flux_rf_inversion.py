9329aa66c626a7805c41a87766a6bc54066ab84c
Kai Katsumata <imperialroyalguard38@gmail.com>
2024-10-23 10:57:10 +0900

    debug

--- pipeline_flux_rf_inversion.py
+++ pipeline_flux_rf_inversion.py
@@ -558,16 +558,16 @@ class FluxRFInversionPipeline(DiffusionPipeline, FluxLoraLoaderMixin):
             image_latents = torch.cat([image_latents], dim=0)
         noise = randn_tensor(shape, generator=generator, device=device, dtype=dtype)
         # latents = noise
-        # latents = self.scheduler.scale_noise(image_latents, timestep, noise)        
-        # latents = self._pack_latents(latents, batch_size, num_channels_latents, height, width)
+        latents = self.scheduler.scale_noise(image_latents, timestep, noise)        
+        latents = self._pack_latents(latents, batch_size, num_channels_latents, height, width)
         # image_latents = self._pack_latents(image_latents, batch_size, num_channels_latents, height, width)
         # print((latents - image_latents).abs().mean())
         image_latents = self._pack_latents(image_latents, batch_size, num_channels_latents, height, width)
         ori_image_latents = image_latents.clone()
-        latents = self.controlled_forward_ode(
-            image_latents, 
-            latent_image_ids, 
-            sigmas, gamma=gamma, null_prompt_embeds=null_prompt_embeds, null_pooled_prompt_embeds=null_pooled_prompt_embeds, null_text_ids=null_text_ids, timesteps=timesteps, guidance=guidance)
+        # latents = self.controlled_forward_ode(
+            # image_latents, 
+            # latent_image_ids, 
+            # sigmas, gamma=gamma, null_prompt_embeds=null_prompt_embeds, null_pooled_prompt_embeds=null_pooled_prompt_embeds, null_text_ids=null_text_ids, timesteps=timesteps, guidance=guidance)
         return ori_image_latents, latents, latent_image_ids
     
     def controlled_forward_ode(self, image_latents, latent_image_ids, sigmas, gamma, null_prompt_embeds, null_pooled_prompt_embeds, null_text_ids, timesteps, guidance):
@@ -921,14 +921,14 @@ class FluxRFInversionPipeline(DiffusionPipeline, FluxLoraLoaderMixin):
                 # diffusion = torch.sqrt(2 * (1-t_i) * (1-eta) / t_i)
                 # noise = torch.randn_like(latents)
 
-                # latents = latents + v_hat_t * (self.scheduler.sigmas[i] -  self.scheduler.sigmas[i + 1])
+                latents = latents + v_hat_t * (self.scheduler.sigmas[i] -  self.scheduler.sigmas[i + 1])
                 # print(t_i, timestep / 1000, dt, eta_t, v_t.mean().item(), latents.mean().item(), v_hat_t.mean().item(), diffusion.mean().item(), noise.mean().item())
                 # if start_timestep <= i < stop_timestep:
                 #    latents = latents + v_hat_t * dt + diffusion * torch.sqrt(dt) * noise
                 # else:
                 #    latents = latents + v_hat_t * dt
                 # print(sigmas, self.scheduler.sigmas)
-                latents = self.scheduler.step(v_hat_t, t, latents, return_dict=False)[0]
+                # latents = self.scheduler.step(v_hat_t, t, latents, return_dict=False)[0]
 
                 # debug print save image 
                 # debug_latents = self._unpack_latents(latents, height, width, self.vae_scale_factor)
